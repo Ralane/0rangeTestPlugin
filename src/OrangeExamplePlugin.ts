251d7724355d70f80e40d0b3eee95b62c7826844
Riley Lane <rileylanea@gmail.com>
2025-07-19 21:17:29 -0700

    Orange example plugin init

--- src/OrangeExamplePlugin.ts
+++ src/OrangeExamplePlugin.ts
@@ -6,8 +6,8 @@ import styles from "../resources/css/base.css";
 import img from "../resources/images/image.png";
 import snd from "../resources/sounds/sound.mp3";
 
-class ExamplePlugin extends Plugin {
-    pluginName = "ExamplePlugin";
+class OrangeExamplePlugin extends Plugin {
+    pluginName = "OrangeExamplePlugin";
     author: string = "Your Name";
 
     constructor() {
@@ -18,16 +18,16 @@ class ExamplePlugin extends Plugin {
     }
 
     start(): void {
-        this.log("ExamplePlugin started");
+        this.log("0rangeYouGlad ExamplePlugin started");
         
         document.getElementById("main")!.innerHTML = blah;
     }
 
     stop(): void {
-        this.log("ExamplePlugin stopped");
+        this.log("0rangeYouGlad ExamplePlugin stopped");
     }
 }
 
 // Export both as default and named export for maximum compatibility
-export default ExamplePlugin;
-export { ExamplePlugin };
+export default OrangeExamplePlugin;
+export { OrangeExamplePlugin };
